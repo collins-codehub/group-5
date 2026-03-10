0ffb8a99dc7760cfdbdce0ffa8f3fca1e3888310
Ghost-monger <emmanuelmusyoka898@gmail.com>
2026-03-10 13:51:19 +0300

    Account-file

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/frank/group-5/venv
+set -gx VIRTUAL_ENV /home/emobilis/group-5/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
